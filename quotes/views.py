391f83e2af29044be76ce287af1438b6e7d8b778
jedctan <jed.ctan@gmail.com>
2025-02-06 10:45:12 -0500

    Step 1 attempt

---
+++ quotes/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    '''
+    Define a view to handle the 'home' request.
+    '''
+
+    response_text = '''
+    <html>
+    <h1>Hello, world!</h1>
+
+    </html>
+    '''
+    
+    return HttpResponse(response_text)
+    
+def show_all(request):
+    '''
+    Define a view to handle the 'home' request.
+    '''
+
+    response_text = '''
+    <html>
+    <h1>Hello, world!</h1>
+
+    </html>
+    '''
+    
+    return HttpResponse(response_text)
+    
+
+def about(request):
+    '''
+    Define a view to handle the 'home' request.
+    '''
+
+    response_text = '''
+    <html>
+    <h1>Hello, world!</h1>
+
+    </html>
+    '''
+    
+    return HttpResponse(response_text)
+    
