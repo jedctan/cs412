0e89d4b913969c388605d33a29ceef08bb1257fb
jedctan <jed.ctan@gmail.com>
2025-02-09 18:10:34 -0500

    Random image and quote functionality

--- quotes/views.py
+++ quotes/views.py
@@ -5,8 +5,12 @@ import random
 
 # Global vars
 
-quotes = []
-images = []
+quotes = ["I feel like if you put something out into the universe, then you increase your chances of it happening.",
+          "There was times when I was bullied about dancing and stuff. But you couldn't hit me hard enough to stop me from doing it.",
+          "I feel like I've been playing Spider-Man my whole life. He's a character I've been pretending to be in my bedroom since I was a kid - so I've been preparing for this forever, I think."
+          ]
+
+images = ['tom1.jpg', 'tom2.jpg', 'tom3.jpg']
 
 # Create your views here.
 
@@ -26,10 +30,12 @@ def quote(request):
     '''
     Define a view to handle the 'quote' request.
     '''
+    rand_ind = random.randint(0,2)
 
     context = {
         "time": time.ctime(),
-        "r_num": chr(random.randint(65,98)),
+        "rand_quote": quotes[rand_ind],
+        "rand_image": images[rand_ind]
     }
     template_name = 'quotes/quote.html'
     
