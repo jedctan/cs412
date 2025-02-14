90f205e3c15765117d401673b44cda0b706af5bb
jedctan <jed.ctan@gmail.com>
2025-02-13 23:19:41 -0500

    Base functionality added for a4

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from django.conf import settings
 from . import views
 
+# URL patterns that are supported by this app:
 urlpatterns = [ 
-    path(r'', views.show_form, name="show_form"),
-    path(r'submit', views.submit, name="submit"),
+    path(r'', views.main, name="main_page"),
+    path(r'main', views.main, name="main_page"),
+    path(r'order', views.order, name="order"),
+    path(r'confirmation', views.confirmation, name="confirmation"),
+
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -2,19 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def show_form(request):
-    '''Show the web page with the form.'''
+def main(request):
+    '''Function to show the main page of the website to describe the restaurant.'''
+    template_name = "restaurant/main.html" # template that contains the main page HTML file
+    return render(request, template_name) # renders the HTML file using the HTTP request and the specified template
 
-    template_name = "restaurant/order.html"
-    return render(request, template_name)
 
-def submit(request):
-    '''Process the form submission, and generate a result.'''
+def order(request):
+    '''Function that responds to the user's get request to show the web page with the form.'''
 
-    template_name = "restaurant/confirmation.html"
+    template_name = "restaurant/order.html" # template that contains the order HTML file
+    return render(request, template_name) # renders the HTML file using the HTTP request and the specified template
+
+def confirmation(request):
+    '''Function to handle a form submission and generate a result.'''
+
+    template_name = "restaurant/confirmation.html" # template that contains teh order HTML file
 
     # read the form data into python variables:
-    if request.POST:
+    # first check if the post request exists
+    if request.POST: 
 
         name = request.POST['name']
         favorite_color = request.POST['favorite_color']
