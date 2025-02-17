e6ee1db7959d88267c8898248ce0b06c3028bf79
jedctan <jed.ctan@gmail.com>
2025-02-16 20:46:10 -0500

    Basic styling and functionality of show all profiles page

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ShowAllProfileViews # our view class definition 
 
 urlpatterns = [
-    path('', ShowAllProfileViews.as_view(), name="show_all")
+    path('', ShowAllProfileViews.as_view(), name="show_all_profiles")
 ]
